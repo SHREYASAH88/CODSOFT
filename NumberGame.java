9946492026ead48b8970fef77651b3b540784a03
SHREYASAH88 <113011958+SHREYASAH88@users.noreply.github.com>
2023-10-19 21:18:11 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,41 +1,56 @@
 package TASK;
-import java.util.*;
-class NumberGame {
+import java.util.Random;
+import java.util.Scanner;
+                                // TASK 1 : NUMBER GAME
+class Game{
+    public int n;
+    public int userInputNumber;
+    public int noOfGuesses=0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int i,r,a=0;
-		Random random = new Random(); 
-		r=random.nextInt(101);
-		System.out.println("GENERATE A RANDOM NUMBER  ="+r);
-		for(i=1;i<6;i++)
-		{
-		System.out.println("ENTER ANY NUMBER");
-		int num=sc.nextInt();
-		a++;
-		if(num==r)
-		{
-			
-			System.out.println("CORRECT NUMBER");
-			
-			break;
-		}
-		else if(num>r&&num<=100)
-		{
-			System.out.println(" TO HIGH");
-			
-		}
-		else
-		{
-			System.out.println("TOO LOW");
-			
-		}
+    public int getNoOfGuesses(){
+        return noOfGuesses;
+    }
+    public void setNoOfGuesses(int noOfGuesses){
+        this.noOfGuesses=noOfGuesses;
+    }
 
-	}
-		System.out.println("USER SCORE");
-		System.out.println("NO OF ATTEMPT ="+a);
-	}
-	
+    Game(){
+        Random r = new Random();
+        this.n = r.nextInt(100);
+    }
 
+    void TakeInput(){
+        System.out.print("Enter your no:");
+        Scanner sc= new Scanner(System.in);
+        userInputNumber=sc.nextInt();
+    }
+    boolean checkCorrectNumber(){
+        noOfGuesses++;
+        if(userInputNumber==n){
+            System.out.format("Yes!You guessed the number\nYou guessed it in %d attempts",noOfGuesses);
+            return true;
+        }
+        else if(userInputNumber<n){
+            System.out.println("It's too low...");
+        }
+        else if (userInputNumber>n){
+            System.out.println("It's too high...");
+        }
+        return false;
+    }
+}
+
+public class NumberGame {
+    public static void main(String[]args){
+        System.out.println("------------------------------------Let's start the game-----------------------------------------------");
+        System.out.println();
+        Game g = new Game();
+        boolean b= false;
+        while(!b){
+            g.TakeInput();
+            b=g.checkCorrectNumber();
+        }
+        System.out.println();
+        System.out.println("\nThank You for playing & i hope you enjoyed it.");
+    }
 }
